5a0ebe0151b365c01423d002d07a434835b4ff64
Alexander Sysuiev <alexander.sysuyev@anahoret.com>
2012-12-18 19:07:10 +0200

    Copy paste som things. Need to overcome laziness

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
-class ItemsController < ApplicationController
-	def index
-		@items = Item.all
-	end
+class ItemsController < WorkflowController
+  def index
+    @items = Item.all
+  end
 end
---
+++ public/javascripts/pages/flash.js
@@ -0,0 +1,4 @@
+$(function () {
+  $(".flash").effect("pulsate", {times: 1}, 1000);
+  $(".flash").effect("fade", {}, 5000);
+});
